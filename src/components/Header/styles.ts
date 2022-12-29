6ee48442ce67e19b78a7e5464138106d6e48b4b9
Guilherme Spica Luiz <Guilhermespicaluiz@gmail.com>
2022-12-29 08:50:01 -0300

    feat: created Header component

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,32 @@
+import styled, { css } from "styled-components";
+
+export const RootHeader = styled.header`
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    nav {
+      display: flex;
+      gap: 0.5rem;
+
+      a {
+        width: 3rem;
+        height: 3rem;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        border-top: 3px solid transparent;
+        border-bottom: 3px solid transparent;
+
+        color: ${theme["gray-100"]};
+
+        &:hover {
+          border-bottom: 3px solid ${theme["green-500"]};
+        }
+      }
+    }
+  `}
+`;
